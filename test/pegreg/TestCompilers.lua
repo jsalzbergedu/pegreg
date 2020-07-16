9872d0858e045fae3362e08c2ae8457ea0b2abf0
Jacob Salzberg <jssalzbe@ncsu.edu>
2020-07-16 16:53:25 -0400

    Replace ordered choice with demote

--- test/pegreg/TestCompilers.lua
+++ test/pegreg/TestCompilers.lua
@@ -1,4 +1,3 @@
 require("test.pegreg.compiler.TestNFSTToDFST")
 require("test.pegreg.compiler.TestEmitStates")
 require("test.pegreg.compiler.TestCompiler")
-require("test.pegreg.compiler.TestOrderedChoice")
